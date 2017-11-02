d694307feecd742508bb397c579eeff990267649
Paula Scholz <swiftress@outlook.com>
2017-11-01 22:33:13 -0700

    Added FlipPdfViewerControl classes

---
+++ PdfViewerHost/FlipPdfViewerControl/FlipPdfViewerControl.xaml.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Windows.Data.Pdf;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+
+using MetroLog;
+
+
+namespace FlipPdfViewerControl
+{
+    public sealed partial class FlipPdfViewerControl : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Uri Source
+        {
+            get { return (Uri)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register("Source", typeof(Uri), typeof(FlipPdfViewerControl),
+                new PropertyMetadata(null, OnSourceChanged));
+
+        public bool IsZoomEnabled
+        {
+            get { return (bool)GetValue(IsZoomEnabledProperty); }
+            set { SetValue(IsZoomEnabledProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsZoomEnabledProperty =
+            DependencyProperty.Register("IsZoomEnabled", typeof(bool), typeof(FlipPdfViewerControl),
+                new PropertyMetadata(true, OnIsZoomEnabledChanged));
+
+        public int PageCount
+        {
+            get { return (int)GetValue(PageCountProperty); }
+            set { SetValue(PageCountProperty, value);  }
+        }
+
+        public static readonly DependencyProperty PageCountProperty =
+            DependencyProperty.Register("PageCount", typeof(int), typeof(FlipPdfViewerControl),
+            new PropertyMetadata(null));
+
+        public int CurrentPageNumber
+        {
+            get { return (int)GetValue(CurrentPageNumberProperty); }
+            set { SetValue(CurrentPageNumberProperty, value);  }
+        }
+
+        public static readonly DependencyProperty CurrentPageNumberProperty =
+            DependencyProperty.Register("CurrentPageNumberCount", typeof(int), typeof(FlipPdfViewerControl),
+            new PropertyMetadata(null));
+
+        internal ZoomMode ZoomMode
+        {
+            get { return IsZoomEnabled ? ZoomMode.Enabled : ZoomMode.Disabled; }
+        }
+
+        public void ClearPdfImages()
+        {
+            // clear it out
+            PdfPages.Clear();
+            PageCount = 0;
+            Source = null;
+        }
+
+        public void AddPdfImage(BitmapImage img)
+        {
+            if(null != img)
+            {
+                PdfPages.Add(img);
+                PageCount++;
+            }      
+        }
+
+        public void IncrementPage()
+        {
+            if(flipView.SelectedIndex >= 0 && flipView.SelectedIndex < PageCount - 1)
+            {
+                flipView.SelectedIndex += 1;
+            }
+        }
+
+        public void DecrementPage()
+        {
+            if(flipView.SelectedIndex > 0)
+            {
+                flipView.SelectedIndex -= 1;
+            }
+        }
+
+        public void ZoomIn()
+        {
+            if(IsZoomEnabled)
+            {
+                FlipViewItem test = (FlipViewItem)flipView.ContainerFromIndex(flipView.SelectedIndex);
+                var viewer = (ImageViewer)test.ContentTemplateRoot;
+
+                viewer.ZoomIn();
+            }
+        }
+
+        public void ZoomOut()
+        {
+            if(IsZoomEnabled)
+            {
+                FlipViewItem test = (FlipViewItem)flipView.ContainerFromIndex(flipView.SelectedIndex);
+                var viewer = (ImageViewer)test.ContentTemplateRoot;
+
+                viewer.ZoomOut();
+            }
+        }
+
+        public void ZoomReset()
+        {
+            FlipViewItem test = (FlipViewItem)flipView.ContainerFromIndex(flipView.SelectedIndex);
+            var viewer = (ImageViewer)test.ContentTemplateRoot;
+
+            viewer.ZoomReset();
+        }
+
+        public bool AutoLoad { get; set; }
+
+        internal ObservableCollection<BitmapImage> PdfPages
+        {
+            get;
+            set;
+        } = new ObservableCollection<BitmapImage>();
+
+        // Metrolog logger
+        private ILogger log;
+
+        public FlipPdfViewerControl()
+        {
+            this.Background = new SolidColorBrush(Colors.DarkGray);
+
+            Window.Current.CoreWindow.CharacterReceived += CoreWindow_CharacterReceived;
+
+            // Loaded is where we hook up component control event handlers
+            Loaded += FlipPdfViewerControl_Loaded;
+
+            log = LogManagerFactory.DefaultLogManager.GetLogger<FlipPdfViewerControl>();
+
+            this.InitializeComponent();
+        }
+
+        private void FlipPdfViewerControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            flipView.SelectionChanged += FlipView_SelectionChanged;
+        }
+
+        // increment/decrement the page counter
+        private void FlipView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int i = ((FlipView)sender).SelectedIndex;
+            CurrentPageNumber = i + 1;
+        }
+
+        // handle keyboard zoom interaction with the flipView
+        private void CoreWindow_CharacterReceived(CoreWindow sender, CharacterReceivedEventArgs args)
+        {
+            if (args.KeyCode == 43 || args.KeyCode == 61) // plus sign / equals sign
+            {
+                ZoomIn();
+            }
+
+            if (args.KeyCode == 45) // minus sign
+            {
+                ZoomOut();
+            }
+
+            if (args.KeyCode == 8) // enter key
+            {
+
+                ZoomReset();
+            }
+        }
+
+        private static void OnIsZoomEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((FlipPdfViewerControl)d).OnIsZoomEnabledChanged();
+        }
+
+        private static void OnSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((FlipPdfViewerControl)d).OnSourceChanged();
+        }
+
+        private void OnIsZoomEnabledChanged()
+        {
+            OnPropertyChanged(nameof(ZoomMode));
+        }
+
+        private async void OnSourceChanged()
+        {
+            log.Trace("In OnSourceChanged()");
+
+            if(null != Source)
+            {
+                log.Trace(string.Format("Source = {0}", Source.ToString()));
+            }
+            else
+            {
+                log.Trace("Source was null.");
+            }
+           
+            if (AutoLoad && Source != null)
+            {
+                // OnSourceChanged is getting called twice each time through 
+                // the Dependency Property system, so let's
+                // gate LoadAsync() from being called again until Load() is finished.
+                // We'll set AutoLoad to true again at the end of the Load() method.
+                AutoLoad = false;
+
+                log.Trace("AutoLoad is true, about to LoadAsync()");
+                await LoadAsync();
+            }
+        }
+
+        public async Task LoadAsync()
+        {
+            log.Trace("In LoadAsync()");
+
+            PdfPages.Clear();
+
+            if(Source.IsFile || !Source.IsWebUri())
+            {
+                log.Trace("Source.IsFile and not IsWebUri, about to LoadFromLocalAsync.");
+
+                await LoadFromLocalAsync();
+            }
+            else if(Source.IsWebUri())
+            {
+                log.Trace("Source.IsWebUri and not IsFile, about to LoadFromRemoteAsync.");
+
+                await LoadFromRemoteAsync();
+            }
+            else
+            {
+                throw new ArgumentException($"Source '{Source.ToString()}' could not be recognized!");
+            }            
+        }
+
+        private async Task LoadFromRemoteAsync()
+        {
+            HttpClient client = new HttpClient();
+
+            var stream = await client.GetStreamAsync(Source);
+
+            var memStream = new MemoryStream();
+
+            await stream.CopyToAsync(memStream);
+
+            memStream.Position = 0;
+
+            PdfDocument doc = await PdfDocument.LoadFromStreamAsync(memStream.AsRandomAccessStream());
+
+            log.Trace("In LoadFromLocalAsync(), about to call Load()");
+
+            Load(doc);
+        }
+
+        private async Task LoadFromLocalAsync()
+        {
+            StorageFile f = await StorageFile.GetFileFromApplicationUriAsync(Source);
+
+            PdfDocument doc = await PdfDocument.LoadFromFileAsync(f);
+
+            log.Trace("In LoadFromLocalAsync(), about to call Load()");
+
+            Load(doc);
+        }
+
+        private async void Load(PdfDocument pdfDoc)
+        {
+            PdfPages.Clear();
+            
+            // clear out the Source property
+            Source = null;
+
+            PageCount = (int)pdfDoc.PageCount;
+
+            CurrentPageNumber = 1;
+
+            log.Trace(string.Format("In Load(), about to load PdfDocument of {0} pages.", pdfDoc.PageCount));
+
+            for (uint i = 0; i < pdfDoc.PageCount; i++)
+            {
+                BitmapImage image = new BitmapImage();
+                image.CreateOptions = BitmapCreateOptions.None;
+
+                var page = pdfDoc.GetPage(i);
+
+                using (InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream())
+                {
+                    await page.RenderToStreamAsync(stream);
+                    await image.SetSourceAsync(stream);
+                }
+
+                log.Trace(string.Format("About to add image number {0} to PdfPages collection.", i));
+
+                PdfPages.Add(image);
+            }
+
+            log.Trace("Leaving Load(), about to set AutoLoad to true");
+
+            // we've finished loading a document, so set AutoLoad to true to 
+            // enable another load, because the UWP framework calls OnSourceChanged twice
+            // through the DependencyProperty system and it will double load the document
+            // if you don't do this.
+            AutoLoad = true;
+
+            // this prevents the control from loading again unless called again
+            // with a new source uri.
+            Source = null;
+
+        }
+
+        public void OnPropertyChanged([CallerMemberName]string property = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
---
+++ PdfViewerHost/FlipPdfViewerControl/imageviewer.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+using MetroLog;
+
+namespace FlipPdfViewerControl
+{
+
+    /// <summary>
+    /// This object replaces the Scrollviewer/Image combination and presents the PDF
+    /// image at its actual aspect ratio, while allowing zoom.  http://www.mobilemotion.eu/?p=2212
+    /// </summary>
+    public class ImageViewer : Grid
+    {
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register(
+        "Source", typeof(ImageSource), typeof(ImageViewer),
+        new PropertyMetadata(default(BitmapImage), SourceChangedCallback));
+
+        private ScrollViewer scroll;
+        private double imgActualWidth;
+        private double imgActualHeight;
+
+        private static int pixelWidth;
+        private static int pixelHeight;
+
+        private static float zoomChangeFactor = 0.2f;
+
+        private static ILogger log = LogManagerFactory.DefaultLogManager.GetLogger<ImageViewer>();
+
+        private static void SourceChangedCallback(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            var control = sender as ImageViewer;
+            var source = args.NewValue as BitmapImage;
+
+            pixelWidth = source.PixelWidth;
+            pixelHeight = source.PixelHeight;
+
+            control.scroll = new ScrollViewer();
+            control.scroll.ZoomMode = ZoomMode.Enabled;
+            control.scroll.Name = "flipScrollViewer";
+            control.scroll.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+            control.scroll.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+
+            var img = new Image
+            {
+                Source = source,
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+
+            img.Loaded += (o, eventArgs) =>
+            {
+                var ratioWidth = control.scroll.ViewportWidth / pixelWidth;
+                var ratioHeight = control.scroll.ViewportHeight / pixelHeight;
+
+                //log.Trace(string.Format("Inside ImageViewer.SourceChangedCallback:img.Loaded. ratioWidth = {0}  ratioHeight = {1}", ratioWidth, ratioHeight));
+
+                var zoomFactor = (ratioWidth >= 1 && ratioHeight >= 1)
+                    ? 1F
+                    : (float)(Math.Min(ratioWidth, ratioHeight));
+
+                //log.Trace(string.Format("Inside ImageViewer.SourceChangedCallback:img.Loaded. zoomFactor = {0}", zoomFactor));
+
+                control.imgActualWidth = img.ActualWidth;
+                control.imgActualHeight = img.ActualHeight;
+
+                control.scroll.ChangeView(null, null, zoomFactor);
+            };
+
+            control.scroll.Content = img;
+            control.Children.Clear();
+            control.Children.Add(control.scroll);
+        }
+
+        public ImageSource Source
+        {
+            get { return (ImageSource)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public void ZoomIn()
+        {
+            double horzOffset = 0;
+            double vertOffset = 0;
+
+            float newZoom = scroll.ZoomFactor + zoomChangeFactor;
+
+            // the pixel size of the content in the scrollviewer now
+            float scaledContentW = (float)imgActualWidth * newZoom;
+            float scaledContentH = (float)imgActualHeight * newZoom;
+
+            // if our content, scaled by the new zoom, is bigger than the viewport, adjust the scroll offset
+            if (scaledContentW < scroll.ViewportWidth)
+            {
+                horzOffset = 0;
+            }
+            else
+            {
+                horzOffset = (scaledContentW - scroll.ViewportWidth) / 2;
+            }
+
+            if (scaledContentH < scroll.ViewportHeight)
+            {
+                vertOffset = 0;
+            }
+            else
+            {
+                vertOffset = (scaledContentH - scroll.ViewportHeight) / 2;
+            }
+
+            scroll.ChangeView(horzOffset, vertOffset, newZoom);
+        }
+
+        public void ZoomOut()
+        {
+            double horzOffset = 0;
+            double vertOffset = 0;
+
+            float newZoom = scroll.ZoomFactor - zoomChangeFactor;
+
+            // the pixel size of the content in the scrollviewer now
+            float scaledContentW = (float)imgActualWidth * newZoom;
+            float scaledContentH = (float)imgActualHeight * newZoom;
+
+            // if our content, scaled by the new zoom, is bigger than the viewport, adjust the scroll offset
+            if (scaledContentW > scroll.ViewportWidth)
+            {
+                horzOffset = (scaledContentW - scroll.ViewportWidth) / 2;
+            }
+            else
+            {
+                horzOffset = 0;
+            }
+
+            if (scaledContentH > scroll.ViewportHeight)
+            {
+                vertOffset = (scaledContentH - scroll.ViewportHeight) / 2;
+            }
+            else
+            {
+                vertOffset = 0;
+            }
+
+            scroll.ChangeView(horzOffset, vertOffset, newZoom);
+        }
+
+        public void ZoomReset()
+        {
+            ImageReset();
+        }
+
+        private void ImageReset()
+        {
+            var ratioWidth = scroll.ViewportWidth / imgActualWidth;
+            var ratioHeight = scroll.ViewportHeight / imgActualHeight;
+
+            var zoomFactor = (ratioWidth >= 1 && ratioHeight >= 1)
+                ? 1F
+                : (float)(Math.Min(ratioWidth, ratioHeight));
+
+            scroll.ChangeView(null, null, zoomFactor);
+        }
+    }
+}
+
